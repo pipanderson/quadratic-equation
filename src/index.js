ac41d7de49340f4d65b991ebb175f6d229589b37
Vlad Soltan <31858445+pipanderson@users.noreply.github.com>
2018-02-27 23:48:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  function compare(first_numb,second_numb) {
+    return first_numb-second_numb;
+  };
+  var x1,x2,solutions=[];
+  var reg=/.\d+/g;
+  var EquationWithoutSpaces= equation.replace(/\s+/g,"");
+  rez=EquationWithoutSpaces.match(reg);
+  a=rez[0];
+  b=rez[2];
+  c=rez[3];
+  var D=(b*b)-(4*a*c);
+  if (D>0) {
+    solutions[0]=Math.round((-b-Math.sqrt(D))/(2*a));
+    solutions[1]=Math.round((-b+Math.sqrt(D))/(2*a));
+    
+    return (solutions.sort(compare));
+  } else if (d=0) {
+    solutions[0]=Math.round(-b/(2*a));
+    return (solutions.sort(compare));
+  } else {
+    return(null);
+  };
+  
